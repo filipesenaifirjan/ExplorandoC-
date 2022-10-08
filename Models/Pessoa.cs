7a77a262497c03a5e5193505d487cc1808390579
Filipe de Mattos Assunção <filipe.assuncao@t2mlab.com>
2022-10-08 18:57:26 -0300

    Criando metodo e validando get e set

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,7 +7,22 @@ namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        private string _nome;//é o campo que vai armazenar o nome
+        public string Nome {
+             get{
+                return _nome.ToUpper();//quero que o nome venha em maiusculo
+            } 
+             set{
+                if (value == ""){
+                    throw new ArgumentException("O nome não pode ficar vazio");
+                }
+                _nome = value;
+            }
+              }
         public int Idade { get; set; }
+
+        public void Apresentar(){
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
     }
-}
+}//o get obtem o valor  e o set atribui esse valor
