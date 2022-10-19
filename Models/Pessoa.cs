57c5a832549b040462fb1688c58fa6ce5fd39e14
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-19 07:45:46 -0300

    validando idade

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -8,10 +8,10 @@ namespace ExemploExplorando.Models
     public class Pessoa
     {
         private string _nome;//é o campo que vai armazenar o nome
+        private int _idade;
         public string Nome {
-             get{
-                return _nome.ToUpper();//quero que o nome venha em maiusculo
-            } 
+             get => _nome.ToUpper();//quero que o nome venha em maiusculo e aqui estou usando o Body expression que é usado em variáveis simples(apenas uma linha)
+            
              set{
                 if (value == ""){
                     throw new ArgumentException("O nome não pode ficar vazio");
@@ -19,7 +19,19 @@ namespace ExemploExplorando.Models
                 _nome = value;
             }
               }
-        public int Idade { get; set; }
+        public int Idade { 
+            get => _idade;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que 0");
+                }
+                _idade = value;
+            } 
+
+            }
 
         public void Apresentar(){
             Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using ExemploExplorando.Models;
 
 Pessoa p1 = new Pessoa();
-p1.Nome = ""; //"Filipe";
+p1.Nome = "Filipe"; //"Filipe";
 p1.Idade = 19;
 p1.Apresentar();
